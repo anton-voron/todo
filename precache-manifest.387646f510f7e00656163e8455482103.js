4bf8de79152d5cdc14b2c2667a32d1d574ac5a45
anton-voron <anton.voron.gt@gmail.com>
2019-06-02 18:54:39 +0300

    Updates

---
+++ precache-manifest.387646f510f7e00656163e8455482103.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985322a22b12a6910efd0db3d4810945",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "7c4b54e497ecff32c436",
+    "url": "/todo/static/css/main.1552f8f5.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/todo/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "7c4b54e497ecff32c436",
+    "url": "/todo/static/js/main.5ff19a21.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
